584210cdafffe59c0e7ec08310c30c05fbf55464
fedorverba <fedor.verba@gmail.com>
2016-11-16 17:34:48 +0300

    Initial commit

---
+++ Frac.java
@@ -0,0 +1,81 @@
+/**
+ * Created by Fedor on 08.11.2016.
+ */
+public class Frac {
+    int a, b;
+    Frac(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+    Frac(int a){
+        this.a=a;
+        this.b=1;
+    }
+    Frac(){
+        this.a=0;
+        this.b=1;
+    }
+    Frac sum (Frac d2) {
+        return new Frac(this.a * d2.b + d2.a * this.b,
+                this.b * d2.b);
+    }
+    Frac sum (int x) {
+        Frac d2=new Frac(x);
+        return new Frac(this.a * d2.b + d2.a * this.b,
+                this.b * d2.b);
+    }
+    Frac mult (Frac d2){
+        return new Frac(this.a*d2.a, this.b*d2.b);
+    }
+    Frac mult (int x){
+        Frac d2=new Frac(x);
+        return new Frac(this.a*d2.a, this.b*d2.b);
+    }
+    Frac div (Frac d2){
+        return new Frac(this.a*d2.b,this.b*d2.a);
+    }
+    Frac div (int x){
+        Frac d2=new Frac(x);
+        return new Frac(this.a*d2.b,this.b*d2.a);
+    }
+    Frac inverse(){
+        return new Frac(this.b,this.a);
+    }
+    boolean isDenumeratorZero(){
+        boolean x=false;
+        if (this.a==0) x=true;
+        return x;
+    }
+    boolean isShorten(){
+        int cifra=1;
+        for (int i=2;i<=a;i++){
+            if (a%i==0&&b%i==0&&i>cifra) return true;
+        }
+        return false;
+    }
+    Frac makeShort(){
+        int cifra=1;
+        for (int i=2;i<=a;i++){
+            if (a%i==0&&b%i==0&&i>cifra) cifra=i;
+        }
+        return new Frac(a/cifra,b/cifra);
+    }
+    boolean isEqual(Frac a){
+        Frac x=new Frac(this.a, this.b);
+        Frac y=a.makeShort();
+        x=x.makeShort();
+        if (x==y) return true;
+        return false;
+    }
+    int compareTo(Frac a){
+        Frac x=new Frac(this.a, this.b);
+        Frac y=a.makeShort();
+        x=x.makeShort();
+        if ((x.a>y.a&&x.b==y.b)||(x.a>=y.a&&x.b<y.b)) return 1;
+        if (x==y) return 0;
+        return -1;
+    }
+    public String toString(){
+        return a+"/"+b;
+    }
+}
